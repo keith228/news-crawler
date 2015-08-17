94fc381aa5b8f35ea8288584f029aae4130f3b84
keith228 <wangzihuan87@hotmail.com>
2015-08-18 00:19:32 +0900

    add comment_json.py

---
+++ tutorial/spiders/comment_json.py
@@ -0,0 +1,33 @@
+import urllib2
+import json
+
+def GetMiddleStr(content,startStr,endStr):
+    startIndex = content.index(startStr)
+    if startIndex >= 0:
+        startIndex += len(startStr)
+    endIndex = content.index(endStr)
+    return content[startIndex: endIndex]
+
+
+response = urllib2.urlopen(r'http://comment.news.163.com/cache/newlist/news_guonei8_bbs/B17R40U90001124J_1.html')
+#comment json url is encoded by utf-8
+html_utf = response.read()
+
+#transfer to std json format
+js = GetMiddleStr(html_utf,'var newPostList={"newPosts":','}],')
+js_0 = js.replace('{"d":0,"1":','')
+js_1 = js_0.replace('}},','},')
+news_json = js_1 + ']'
+
+'''
+key : value
+f : user location
+d : news code
+b : comment content
+n : user name
+t : time
+ect
+'''
+hjson = json.loads(news_json, encoding ="utf-8")
+for items in hjson:
+    print items['b']
